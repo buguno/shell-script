d2d36b7423fa15a82af3c60b02e2f8d46b950aac
buguno <brunnodesouzabezerra@gmail.com>
2022-07-21 21:27:45 -0300

    feat: creating reserved variables sh

---
+++ module 01/reserved-variables.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Parameter 1: $1"
+echo "Parameter 2: $2"
+
+echo "All parameters: $*"
+
+echo "Number of parameters: $#"
+
+echo "Output of the last command: $?"
+
+echo "PID of the current process: $$"
+
+echo $0
